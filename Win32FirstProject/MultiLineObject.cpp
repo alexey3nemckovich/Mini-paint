64417c4e5f29f8f64335a8c782c55588454e7270
alexey3nemckovich <alexey3nemckovich@gmail.com>
2016-09-12 23:46:18 +0300

    Modified pencil drawing

--- Win32FirstProject/MultiLineObject.cpp
+++ Win32FirstProject/MultiLineObject.cpp
@@ -8,10 +8,21 @@ MultiLineObject::MultiLineObject(HPEN hPen, HBRUSH hBrush) : DrawingObject(hPen,
 void MultiLineObject::draw(HDC hdc, bool redraw)
 {
 	HGDIOBJ prevPen = SelectObject(hdc, hPen);
+	POINT prevPos; 
+	MoveToEx(hdc, 0, 0, &prevPos);
 	int coordinatesCount = coordinates.size();
-	for (int i = 0; i < coordinatesCount; i++)
+	if (coordinatesCount == 1)
 	{
-		Ellipse(hdc, coordinates[i].x, coordinates[i].y, coordinates[i].x + 2, coordinates[i].y + 2);
+		Ellipse(hdc, coordinates[0].x - 1, coordinates[0].y - 1, coordinates[0].x + 1, coordinates[0].y + 1);
 	}
+	else
+	{
+		for (int i = 1; i < coordinatesCount; i++)
+		{
+			MoveToEx(hdc, coordinates[i-1].x, coordinates[i-1].y, NULL);
+			LineTo(hdc, coordinates[i].x, coordinates[i].y);
+		}
+	}
+	MoveToEx(hdc, prevPos.x, prevPos.y, NULL);
 	SelectObject(hdc, prevPen);
 }
